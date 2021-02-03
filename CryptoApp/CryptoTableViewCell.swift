7abbadf6433737fbf354b2c1c5d926284c734105
onurbasdas <onurebasdase@hotmail.com>
2021-02-03 22:54:54 +0300

    first commit

---
+++ CryptoApp/CryptoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CryptoTableViewCell.swift
+//  CryptoApp
+//
+//  Created by Onur Başdaş on 3.02.2021.
+//
+
+import UIKit
+
+class CryptoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
