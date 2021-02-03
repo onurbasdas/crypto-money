b8194d37621fd0a196c78ed0e0bb210fa397a8df
onurbasdas <onurebasdase@hotmail.com>
2021-02-03 23:47:39 +0300

    add view model completed

---
+++ CryptoApp/ViewModel/CryptoViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  CryptoViewModel.swift
+//  CryptoApp
+//
+//  Created by Onur Başdaş on 3.02.2021.
+//
+
+import Foundation
+struct CryptoListViewModel {
+    let cryptoCurrencyList : [CryptoCurrency]
+    func numberOfRowsInSection() -> Int {
+        return self.cryptoCurrencyList.count
+    }
+    func cryptoAtIndex(_ index : Int) -> CryptoViewModel{
+        let crypto = self.cryptoCurrencyList[index]
+        return CryptoViewModel(cryptoCurrency: crypto)
+    }
+}
+struct CryptoViewModel {
+    let cryptoCurrency : CryptoCurrency
+    
+    var name : String{
+        return self.cryptoCurrency.currency
+    }
+    var price : String{
+        return self.cryptoCurrency.price
+    }
+}
